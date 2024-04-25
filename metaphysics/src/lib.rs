c3ce87cda73ed785f81e0b68349c8f4fa847fe59
2-3-5-41 <93025618+2-3-5-41@users.noreply.github.com>
2024-04-24 21:15:09 -0400

    creating a pipeline for thread communication

--- metaphysics/src/lib.rs
+++ metaphysics/src/lib.rs
@@ -1,7 +1,16 @@
-use std::sync::Arc;
+use std::{num::NonZeroU32, sync::Arc, time::Duration};
 
 use godot::prelude::*;
-use metaphy_network::{init_debug_interface, libp2p::futures::StreamExt};
+use metaphy_network::{
+    init_debug_interface,
+    libp2p::{
+        core::{transport::ListenerId, ConnectedPoint},
+        futures::StreamExt,
+        swarm::{ConnectionError, ConnectionId, DialError, ListenError, SwarmEvent},
+        Multiaddr, PeerId, TransportError,
+    },
+    Logic,
+};
 use tokio::{
     runtime::{self, Runtime},
     select,
@@ -17,6 +26,7 @@ unsafe impl ExtensionLibrary for Metaphysics {}
 pub struct Metaphy {
     base: Base<Node>,
     runtime: Arc<Runtime>,
+    swarm_event_channel: Option<tokio::sync::mpsc::Receiver<SwarmThreadEvent>>,
 }
 
 #[godot_api]
@@ -30,17 +40,26 @@ impl INode for Metaphy {
                     .build()
                     .expect("Failed to create Tokio Runtime!"),
             ),
+            swarm_event_channel: None,
         }
     }
 
     fn ready(&mut self) {
         init_debug_interface();
 
-        self.runtime().spawn(async {
+        // Create our channels for communication between the swarm/network thread and the main Godot thread.
+        let (events_tx, events_rx) = tokio::sync::mpsc::channel::<SwarmThreadEvent>(32);
+
+        // Creating a thread for our Node & Swarm.
+        self.runtime().spawn(async move {
+            let channel = events_tx;
+
             let node = metaphy_network::Phylosopher::new(None).expect("Failed to create P2P Node!");
             node.bind(None).await;
 
             let swarm = node.get_swarm();
+
+            // let _ = channel.send(None);
 
             loop {
                 select! {
@@ -48,27 +67,89 @@ impl INode for Metaphy {
                         let mut swarm = swarm.lock().await;
                         swarm.select_next_some().await
                     } => {
-                        match swarm_event {
-                            metaphy_network::libp2p::swarm::SwarmEvent::Behaviour(behaviour) => todo!(),
-                            metaphy_network::libp2p::swarm::SwarmEvent::ConnectionEstablished { peer_id, connection_id, endpoint, num_established, concurrent_dial_errors, established_in } => todo!(),
-                            metaphy_network::libp2p::swarm::SwarmEvent::ConnectionClosed { peer_id, connection_id, endpoint, num_established, cause } => todo!(),
-                            metaphy_network::libp2p::swarm::SwarmEvent::IncomingConnection { connection_id, local_addr, send_back_addr } => todo!(),
-                            metaphy_network::libp2p::swarm::SwarmEvent::IncomingConnectionError { connection_id, local_addr, send_back_addr, error } => todo!(),
-                            metaphy_network::libp2p::swarm::SwarmEvent::OutgoingConnectionError { connection_id, peer_id, error } => todo!(),
-                            metaphy_network::libp2p::swarm::SwarmEvent::NewListenAddr { listener_id, address } => todo!(),
-                            metaphy_network::libp2p::swarm::SwarmEvent::ExpiredListenAddr { listener_id, address } => todo!(),
-                            metaphy_network::libp2p::swarm::SwarmEvent::ListenerClosed { listener_id, addresses, reason } => todo!(),
-                            metaphy_network::libp2p::swarm::SwarmEvent::ListenerError { listener_id, error } => todo!(),
-                            metaphy_network::libp2p::swarm::SwarmEvent::Dialing { peer_id, connection_id } => todo!(),
-                            metaphy_network::libp2p::swarm::SwarmEvent::NewExternalAddrCandidate { address } => todo!(),
-                            metaphy_network::libp2p::swarm::SwarmEvent::ExternalAddrConfirmed { address } => todo!(),
-                            metaphy_network::libp2p::swarm::SwarmEvent::ExternalAddrExpired { address } => todo!(),
-                            _ => todo!(),
+                        match channel.send(swarm_event.into()).await {
+                            Ok(_) => return,
+                            Err(attempt) => println!("[ERROR] -> Failed to send {attempt:?} over the events channel to main thread!"),
                         }
                     }
                 }
             }
         });
+
+        // Add missing items to our main struct
+        self.swarm_event_channel = Some(events_rx);
+    }
+
+    fn physics_process(&mut self, _deta: f64) {
+        match self
+            .swarm_event_channel
+            .as_mut()
+            .expect("There is no events channel to read from!")
+            .try_recv()
+        {
+            Ok(event) => match event {
+                SwarmThreadEvent::Behvaiour(event) => godot_print!("{:?}", event),
+                // SwarmThreadEvent::ConnectionEstablished {
+                //     peer_id,
+                //     connection_id,
+                //     endpoint,
+                //     num_established,
+                //     concurrent_dial_errors,
+                //     established_in,
+                // } => todo!(),
+                // SwarmThreadEvent::ConnectionClosed {
+                //     peer_id,
+                //     connection_id,
+                //     endpoint,
+                //     num_established,
+                //     cause,
+                // } => todo!(),
+                // SwarmThreadEvent::IncomingConnection {
+                //     connection_id,
+                //     local_addr,
+                //     send_back_addr,
+                // } => todo!(),
+                // SwarmThreadEvent::IncomingConnectionError {
+                //     connection_id,
+                //     local_addr,
+                //     send_back_addr,
+                //     error,
+                // } => todo!(),
+                // SwarmThreadEvent::OutgoingConnectionError {
+                //     connection_id,
+                //     peer_id,
+                //     error,
+                // } => todo!(),
+                SwarmThreadEvent::NewListenAddress {
+                    listener_id,
+                    address,
+                } => godot_print!("New listen address: {}, {}", listener_id, address),
+                // SwarmThreadEvent::ExpiredListenAddress {
+                //     listener_id,
+                //     address,
+                // } => todo!(),
+                // SwarmThreadEvent::ListenerClosed {
+                //     listener_id,
+                //     addresses,
+                //     reason,
+                // } => todo!(),
+                // SwarmThreadEvent::ListenerError { listener_id, error } => todo!(),
+                // SwarmThreadEvent::Dialing {
+                //     peer_id,
+                //     connection_id,
+                // } => todo!(),
+                // SwarmThreadEvent::NewExternalAddrCandidate { address } => todo!(),
+                // SwarmThreadEvent::ExternalAddrConfirmed { address } => todo!(),
+                // SwarmThreadEvent::ExternalAddrExpired { address } => todo!(),
+                _ => (),
+            },
+            Err(error) => match error {
+                tokio::sync::mpsc::error::TryRecvError::Empty => (),
+                tokio::sync::mpsc::error::TryRecvError::Disconnected => {
+                    panic!("[FETAL] -> The swarm thread event channel has been disconnected!\nThis could've happend do to the swarm thread crashing for some reason...")
+                }
+            },
+        }
     }
 }
 
@@ -76,5 +157,178 @@ impl INode for Metaphy {
 impl Metaphy {
     pub fn runtime(&self) -> Arc<Runtime> {
         Arc::clone(&self.runtime)
+    }
+}
+
+#[derive(Debug)]
+enum SwarmThreadEvent {
+    Behvaiour(Logic),
+    ConnectionEstablished {
+        peer_id: PeerId,
+        connection_id: ConnectionId,
+        endpoint: ConnectedPoint,
+        num_established: NonZeroU32,
+        concurrent_dial_errors: Option<Vec<(Multiaddr, TransportError<std::io::Error>)>>,
+        established_in: Duration,
+    },
+    ConnectionClosed {
+        peer_id: PeerId,
+        connection_id: ConnectionId,
+        endpoint: ConnectedPoint,
+        num_established: u32,
+        cause: Option<ConnectionError>,
+    },
+    IncomingConnection {
+        connection_id: ConnectionId,
+        local_addr: Multiaddr,
+        send_back_addr: Multiaddr,
+    },
+    IncomingConnectionError {
+        connection_id: ConnectionId,
+        local_addr: Multiaddr,
+        send_back_addr: Multiaddr,
+        error: ListenError,
+    },
+    OutgoingConnectionError {
+        connection_id: ConnectionId,
+        peer_id: Option<PeerId>,
+        error: DialError,
+    },
+    NewListenAddress {
+        listener_id: ListenerId,
+        address: Multiaddr,
+    },
+    ExpiredListenAddress {
+        listener_id: ListenerId,
+        address: Multiaddr,
+    },
+    ListenerClosed {
+        listener_id: ListenerId,
+        addresses: Vec<Multiaddr>,
+        reason: Result<(), std::io::Error>,
+    },
+    ListenerError {
+        listener_id: ListenerId,
+        error: std::io::Error,
+    },
+    Dialing {
+        peer_id: Option<PeerId>,
+        connection_id: ConnectionId,
+    },
+    NewExternalAddrCandidate {
+        address: Multiaddr,
+    },
+    ExternalAddrConfirmed {
+        address: Multiaddr,
+    },
+    ExternalAddrExpired {
+        address: Multiaddr,
+    },
+    None,
+}
+
+impl From<SwarmEvent<Logic>> for SwarmThreadEvent {
+    fn from(value: SwarmEvent<Logic>) -> Self {
+        match value {
+            SwarmEvent::Behaviour(event) => Self::Behvaiour(event),
+            SwarmEvent::ConnectionEstablished {
+                peer_id,
+                connection_id,
+                endpoint,
+                num_established,
+                concurrent_dial_errors,
+                established_in,
+            } => Self::ConnectionEstablished {
+                peer_id,
+                connection_id,
+                endpoint,
+                num_established,
+                concurrent_dial_errors,
+                established_in,
+            },
+            SwarmEvent::ConnectionClosed {
+                peer_id,
+                connection_id,
+                endpoint,
+                num_established,
+                cause,
+            } => Self::ConnectionClosed {
+                peer_id,
+                connection_id,
+                endpoint,
+                num_established,
+                cause,
+            },
+            SwarmEvent::IncomingConnection {
+                connection_id,
+                local_addr,
+                send_back_addr,
+            } => Self::IncomingConnection {
+                connection_id,
+                local_addr,
+                send_back_addr,
+            },
+            SwarmEvent::IncomingConnectionError {
+                connection_id,
+                local_addr,
+                send_back_addr,
+                error,
+            } => Self::IncomingConnectionError {
+                connection_id,
+                local_addr,
+                send_back_addr,
+                error,
+            },
+            SwarmEvent::OutgoingConnectionError {
+                connection_id,
+                peer_id,
+                error,
+            } => Self::OutgoingConnectionError {
+                connection_id,
+                peer_id,
+                error,
+            },
+            SwarmEvent::NewListenAddr {
+                listener_id,
+                address,
+            } => Self::NewListenAddress {
+                listener_id,
+                address,
+            },
+            SwarmEvent::ExpiredListenAddr {
+                listener_id,
+                address,
+            } => Self::ExpiredListenAddress {
+                listener_id,
+                address,
+            },
+            SwarmEvent::ListenerClosed {
+                listener_id,
+                addresses,
+                reason,
+            } => Self::ListenerClosed {
+                listener_id,
+                addresses,
+                reason,
+            },
+            SwarmEvent::ListenerError { listener_id, error } => {
+                Self::ListenerError { listener_id, error }
+            }
+            SwarmEvent::Dialing {
+                peer_id,
+                connection_id,
+            } => Self::Dialing {
+                peer_id,
+                connection_id,
+            },
+            SwarmEvent::NewExternalAddrCandidate { address } => {
+                Self::NewExternalAddrCandidate { address }
+            }
+            SwarmEvent::ExternalAddrConfirmed { address } => {
+                Self::ExternalAddrConfirmed { address }
+            }
+            SwarmEvent::ExternalAddrExpired { address } => Self::ExternalAddrExpired { address },
+            _ => Self::None,
+        }
     }
 }
